b8307fefab7670f091eac9d7bba70b7db9f906d8
developerakshat12 <148519895+developerakshat12@users.noreply.github.com>
2025-11-26 17:31:21 +0530

    Update Q7

--- Q7.c
+++ Q7.c
@@ -1,61 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX 10000
+#define MAX 1000
 
-int adj[MAX][MAX];         // adjacency matrix (simple for beginners)
+int adj[MAX][MAX];
 int visited[MAX];
 int queue[MAX];
-int front = 0, rear = 0;
-
-void enqueue(int x) { queue[rear++] = x; }
-int dequeue() { return queue[front++]; }
-int isEmpty() { return front == rear; }
 
 int main() {
     int N, M;
     scanf("%d %d", &N, &M);
 
-    // build graph
-    for(int i = 0; i < M; i++) {
+    // Build adjacency matrix
+    for (int i = 0; i < M; i++) {
         int u, v;
         scanf("%d %d", &u, &v);
-        adj[u][v] = adj[v][u] = 1;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
     }
 
     int S;
     scanf("%d", &S);
 
-    visited[S] = 1;
+    // BFS queue
+    int front = 0, rear = 0;
 
-    // BFS
-    enqueue(S);
+    visited[S] = 1;
+    queue[rear++] = S;
 
     int largestTier = 0;
 
-    while(!isEmpty()) {
-        int levelCount = rear - front;  // nodes in current tier
-        if(levelCount == 0) break;
+    while (front < rear) {
 
-        // but skip Tier 0 size calculation
-        int actualCount = 0;
-        for(int i = 0; i < levelCount; i++) {
-            int node = dequeue();
+        int levelSize = rear - front;   // nodes in this tier
+        int nextTierCount = 0;          // nodes in next tier
 
-            // explore neighbors
-            for(int j = 0; j < N; j++) {
-                if(adj[node][j] && !visited[j]) {
+        // Process current tier
+        for (int i = 0; i < levelSize; i++) {
+            int node = queue[front++];
+
+            // Explore neighbors
+            for (int j = 0; j < N; j++) {
+                if (adj[node][j] == 1 && !visited[j]) {
                     visited[j] = 1;
-                    enqueue(j);
-                    actualCount++;
+                    queue[rear++] = j;
+                    nextTierCount++;
                 }
             }
         }
 
-        if(actualCount > largestTier)
-            largestTier = actualCount;
+        // Skip Tier 0 (levelSize = 1 at start)
+        if (nextTierCount > largestTier) {
+            largestTier = nextTierCount;
+        }
     }
 
-    printf("%d", largestTier);
+    printf("%d\n", largestTier);
     return 0;
 }
